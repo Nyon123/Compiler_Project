0dca5237077eeedcc7356aab458e99f86a101def
sakibur151 <sakiburrahman01796@gmail.com>
2021-12-16 20:14:58 +0600

    Second

--- Compiler.cpp
+++ Compiler.cpp
@@ -1,6 +1,37 @@
 #include <iostream>
 using namespace std;
+string removeComments(string prgm)
+{
+    int n = prgm.length();
+    string res;
 
+
+    bool s_cmt = false;
+    bool m_cmt = false;
+
+
+    for (int i=0; i<n; i++)
+    {
+
+        if (s_cmt == true && prgm[i] == '\n')
+            s_cmt = false;
+
+        else if  (m_cmt == true && prgm[i] == '*' && prgm[i+1] == '/')
+            m_cmt = false,  i++;
+
+        else if (s_cmt || m_cmt)
+            continue;
+
+        else if (prgm[i] == '/' && prgm[i+1] == '/')
+            s_cmt = true, i++;
+        else if (prgm[i] == '/' && prgm[i+1] == '*')
+            m_cmt = true,  i++;
+
+
+      else  res += prgm[i];
+    }
+    return res;
+}
 void removeSpaces(string &prgm)
 {
 
